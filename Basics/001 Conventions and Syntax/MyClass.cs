3cfddf8b7018437141b934ec29474e0ea0c1a81f
Jace Bradly Rosenhan <112522810+JBRosenhan@users.noreply.github.com>
2022-09-12 16:09:54 -0600

    Assass Class

--- Basics/001 Conventions and Syntax/MyClass.cs
+++ Basics/001 Conventions and Syntax/MyClass.cs
@@ -7,3 +7,41 @@
 //Add another class with any name, add variables 
 //Using the new keyword create several objects
 //Console writeline the Objects variables
+using System;
+					
+public class Program
+{
+    public Hero assassin;
+	
+    public void Main()
+    {
+        assassin = new Hero();
+        
+		
+        assassin.health = 50;
+        assassin.powerLevel = 10;
+        assassin.armorLevel = 8;
+        assassin.mana = 150;
+        assassin.speed = 12;
+        assassin.attackDMG = 15;
+		
+        
+		Console.WriteLine(assassin.heroClass);
+        Console.WriteLine("Health = " + assassin.health);
+        Console.WriteLine("Power Level = " + assassin.powerLevel);
+        Console.WriteLine("Armor Level = " + assassin.armorLevel);
+        Console.WriteLine("Mana = " + assassin.mana);
+        Console.WriteLine("Speed = " + assassin.speed);
+        Console.WriteLine("Attack Damage = " + assassin.attackDMG);
+    }
+}
+
+public class Hero {
+    public int health;
+    public int powerLevel;
+    public int armorLevel;
+    public int mana;
+    public int speed;
+    public int attackDMG;
+    public string heroClass = "Assassin";
+}
