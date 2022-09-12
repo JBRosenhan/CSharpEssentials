612c4bdaa00c80900ae592f85746f8a28788c607
Jace Bradly Rosenhan <112522810+JBRosenhan@users.noreply.github.com>
2022-09-12 12:04:23 -0600

    Practiced Weapon

--- Basics/001 Conventions and Syntax/Weapons.cs
+++ Basics/001 Conventions and Syntax/Weapons.cs
@@ -4,7 +4,7 @@ public class Program
 {
 	public Weapon miniGun;
 	public PowerUp weaponPowerUp;
-	
+		
 	public void Main()
 	{
 		miniGun = new Weapon();
@@ -13,16 +13,16 @@ public class Program
 		miniGun.ammoCount++;
 		miniGun.firePower += weaponPowerUp.powerLevel;
 		Console.WriteLine(miniGun.weaponName + " has an ammo count of " + miniGun.ammoCount + ".");
-		Console.WriteLine("The fire power of is now " + miniGun.firePower + ", it has increased.");
+		Console.WriteLine("The fire power of it is now " + miniGun.firePower + " has increased.");
 	}
 }
 
-public class Weapon { 
+public class Weapon {
 	public int ammoCount = 10;
 	public string weaponName = "Mini Gun";
 	public float firePower = 11.5f;
 }
 
 public class PowerUp {
-	public float powerLevel = 2.5f;	
+	public float powerLevel = 2.5f;
 }
