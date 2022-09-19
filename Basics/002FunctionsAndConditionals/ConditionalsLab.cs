f263de268d591fada72e560eed32d8787a7a2e60
Jace Bradly Rosenhan <112522810+JBRosenhan@users.noreply.github.com>
2022-09-19 13:35:10 -0600

    My own Work

--- Basics/002FunctionsAndConditionals/ConditionalsLab.cs
+++ Basics/002FunctionsAndConditionals/ConditionalsLab.cs
@@ -2,42 +2,18 @@ using System;
 					
 public class Program
 {
-	public Operations myOperator;
-	
 	public void Main()
 	{
-		myOperator = new Operations();
-		
-		Console.WriteLine("Welcome");
-		myOperator.DoMath(10, 4);
-		myOperator.DoMath(20, 7);
-		myOperator.DoMath(30, 15);
-		myOperator.Compare(4,3);
-		myOperator.Compare(3,4);
-		myOperator.CheckPassword("SevenOF9");
-		myOperator.CheckPassword("OU812");
-	}
-}
-
-public class Operations {
-	public void DoMath (int value, int value2) {
-		var number = value + value2;
-		Console.WriteLine(number);
-	}
-	
-	public void Compare (int value, int value2) {
-		if(value > value2) {
-			Console.WriteLine("True, the first greater.");
-		} else {
-			Console.WriteLine("False, the second is greater.");
-		}
-	}
-	
-	public void CheckPassword (string password) {
-		if(password == "OU812") {
-			Console.WriteLine("Correct Password");
-		} else {
-			Console.WriteLine("Incorrect Password");	
+		int j = 21;
+		int r = 21;
+		if (j > r)
+		{
+		Console.WriteLine("j is greater then r");
+		}else if (j < r)
+		{
+			Console.WriteLine("j is less then r");
+		}else{
+			Console.WriteLine("j is equal to r");
 		}
 	}
 }
