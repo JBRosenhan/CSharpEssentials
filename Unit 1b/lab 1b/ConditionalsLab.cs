2b790994fef2a50d44c74ed70dc268f6df1363ae
Jace Bradly Rosenhan <112522810+JBRosenhan@users.noreply.github.com>
2022-09-20 16:12:22 -0600

    fixed

---
+++ Unit 1b/lab 1b/ConditionalsLab.cs
@@ -0,0 +1,19 @@
+using System;
+					
+public class Program
+{
+	public void Main()
+	{
+		int j = 21;
+		int r = 21;
+		if (j > r)
+		{
+		Console.WriteLine("j is greater then r");
+		}else if (j < r)
+		{
+			Console.WriteLine("j is less then r");
+		}else{
+			Console.WriteLine("j is equal to r");
+		}
+	}
+}
