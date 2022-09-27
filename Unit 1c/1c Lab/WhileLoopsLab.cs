1d803d16994046bd0cd54ccc834925d58a371cf8
Jace Bradly Rosenhan <112522810+JBRosenhan@users.noreply.github.com>
2022-09-26 22:18:37 -0600

    practice the example

--- Unit 1c/1c Lab/WhileLoopsLab.cs
+++ Unit 1c/1c Lab/WhileLoopsLab.cs
@@ -5,26 +5,9 @@ public class Program
 	public void Main()
 	{
 		int i = 0;
-		while (i < 10) {
+		while (i < 5) {
 		Console.WriteLine(i);
 			i++;
 		}
-	}
-}
-
-using System;
-				
-public class program
-{
- 
-	public void Main()
-	{
-	int j = 0;
-	do
-	{
-	Console.WriteLine(j);
-		j++;
-	}
-		while (j < 8);
 	}
 }
